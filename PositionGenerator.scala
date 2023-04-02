730c24520682daef4b6c0f707842f87a794205a7
Thanh Le <lenguyenthanh@hotmail.com>
2023-04-02 13:42:41 +0200

    Include moves when return generated positions

--- PositionGenerator.scala
+++ PositionGenerator.scala
@@ -1,23 +1,49 @@
 import chess.*
-import chess.format.{Fen, EpdFen}
+import chess.format.{Fen, EpdFen, Uci}
 import chess.variant.Variant
+import chess.MoveOrDrop.*
+import cats.data.State
 
 object PositionGenerator:
-  def generate(variant: Variant, moves: Int, positions: Int): List[EpdFen] =
+
+  case class Result(val fen: EpdFen, val moves: List[Uci])
+  def generate(variant: Variant, moves: Int, positions: Int): List[Result] =
     val situation = Situation(variant)
     (1 to positions)
-      .map(_ => situation.gen(moves))
-      .map(Fen.write(_))
+      .map(_ => situation.gen(moves, Nil))
+      .map((sit, moves) => Result(Fen.write(sit), moves.map(_.toUci)))
       .toList
 
+  val nextMove: State[Situation, Option[Move]] = State(sit =>
+    val legalMoves = sit.legalMoves
+    if legalMoves.isEmpty then (sit, None)
+    else
+      val rnd = scala.util.Random.nextInt(legalMoves.size)
+      val move = legalMoves(rnd)
+      (move.situationAfter, Some(move))
+  )
+
+  def gen(moves: Int): State[Situation, List[Move]] =
+    if moves == 0 then State.pure(Nil)
+    else
+      nextMove.flatMap(move =>
+        gen(moves - 1).map(moves => move.toList ++ moves)
+      )
+
   extension (s: Situation)
-    def next: Situation =
+    def next: Option[MoveOrDrop] =
       val legalMoves = s.legalMoves
-      if legalMoves.isEmpty then s
+      if legalMoves.isEmpty then None
       else
         val rnd = scala.util.Random.nextInt(legalMoves.size)
-        legalMoves(rnd).situationAfter
+        Some(legalMoves(rnd))
 
-    def gen(moves: Int): Situation =
-      if moves == 0 then s
-      else s.next.gen(moves - 1)
+    def gen(
+        moves: Int,
+        accumulation: List[MoveOrDrop]
+    ): (Situation, List[MoveOrDrop]) =
+      if moves == 0 then (s, accumulation)
+      else
+        s.next match
+          case None       => (s, accumulation)
+          case Some(move) => gen(moves - 1, accumulation :+ move)
