3cb8277b8f705969caf3f2e1f622e18676b65748
Thanh Le <lenguyenthanh@hotmail.com>
2023-04-07 09:09:28 +0200

    Use Game for generating positions

--- PositionGenerator.scala
+++ PositionGenerator.scala
@@ -1,5 +1,6 @@
 import chess.*
 import chess.format.{ EpdFen, Fen, Uci }
+import chess.format.pgn.SanStr
 import chess.variant.{ Crazyhouse, Variant }
 import chess.MoveOrDrop.*
 import cats.data.State
@@ -8,10 +9,10 @@ object PositionGenerator:
   import Domain.{ Position, PositionGenConfig }
 
   def generate(variant: Variant, config: PositionGenConfig): List[Result] =
-    val situation = Situation(variant)
+    val game = Game(Situation(variant))
     (1 to config.positions)
-      .map(_ => situation.gen(config.moves, Nil))
-      .map((sit, moves) => Result(Position(variant, Fen.write(sit)), moves.map(_.toUci.uci)))
+      .map(_ => game.gen(config.moves))
+      .map(game => Result(Position(variant, Fen.write(game)), game.sans.toList))
       .toList
 
   val nextMove: State[Situation, Option[Move]] = State(sit =>
@@ -23,26 +24,22 @@ object PositionGenerator:
       (move.situationAfter, Some(move))
   )
 
-  def gen(moves: Int): State[Situation, List[Move]] =
-    if moves == 0 then State.pure(Nil)
-    else nextMove.flatMap(move => gen(moves - 1).map(moves => move.toList ++ moves))
-
-  case class Result(position: Position, moves: List[String]):
+  case class Result(position: Position, moves: List[SanStr]):
     override def toString(): String = s"$position;${moves.mkString(" ")}"
 
-  extension (s: Situation)
+  extension (game: Game)
     def next: Option[MoveOrDrop] =
       val legalMoves =
-        if s.board.variant.crazyhouse then Crazyhouse.legalMoves(s)
-        else s.legalMoves
+        if game.board.variant.crazyhouse then Crazyhouse.legalMoves(game.situation)
+        else game.situation.legalMoves
       if legalMoves.isEmpty then None
       else
         val rnd = scala.util.Random.nextInt(legalMoves.size)
         Some(legalMoves(rnd))
 
-    def gen(moves: Int, accumulation: List[MoveOrDrop]): (Situation, List[MoveOrDrop]) =
-      if moves == 0 then (s, accumulation)
+    def gen(moves: Int): Game =
+      if moves == 0 then game
       else
-        s.next match
-          case None       => (s, accumulation)
-          case Some(move) => move.situationAfter.gen(moves - 1, accumulation :+ move)
+        game.next match
+          case None       => game
+          case Some(move) => move.fold(game.apply, game.applyDrop).gen(moves - 1)
