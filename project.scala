abcf0aa205f30365bf606ac1e47804026f52b253
Thanh Le <lenguyenthanh@hotmail.com>
2023-04-10 17:46:38 +0200

    Bump scala & scalachess deps
    Also remove unused os-lib

--- project.scala
+++ project.scala
@@ -1,5 +1,4 @@
-//> using scala "3.2.2"
+//> using scala "3.3.0-RC3"
 //> using repository "https://raw.githubusercontent.com/lichess-org/lila-maven/master"
-//> using lib "org.lichess::scalachess:14.6.3"
+//> using lib "org.lichess::scalachess:14.8.1"
 //> using lib "org.typelevel::toolkit::0.0.4"
-//> using lib "com.lihaoyi::os-lib:0.9.1"
